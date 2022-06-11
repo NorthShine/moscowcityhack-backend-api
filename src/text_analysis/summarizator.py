80020f78afc7c3b94fde2856461420fca9f6e961
s.kuchuganova <s.kuchuganova@expasoft.tech>
2022-06-12 00:28:54 +0700

    text ton added

--- src/text_analysis/summarizator.py
+++ src/text_analysis/summarizator.py
@@ -7,17 +7,15 @@ model = MBartForConditionalGeneration.from_pretrained(model_name)
 
 def get_summary(article_text: str, model=model, tokenizer=tokenizer):
     input_ids = tokenizer(
-        [article_text],
-        max_length=600,
-        padding="max_length",
-        truncation=True,
-        return_tensors="pt",
-    )["input_ids"]
+                [article_text],
+                max_length=600,
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt")["input_ids"]
 
     output_ids = model.generate(
-        input_ids=input_ids,
-        no_repeat_ngram_size=4
-    )[0]
+                input_ids=input_ids,
+                no_repeat_ngram_size=4)[0]
 
     summary = tokenizer.decode(output_ids, skip_special_tokens=True)
     return summary
